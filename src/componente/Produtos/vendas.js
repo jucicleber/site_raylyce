72a9d4b40f54a8a679045935639a0b1636212cc7
jucicleber <71895344+jucicleber@users.noreply.github.com>
2022-08-08 17:31:19 -0400

    att

--- src/componente/Produtos/vendas.js
+++ src/componente/Produtos/vendas.js
@@ -13,11 +13,11 @@ const Vendas = (props) => {
                 <img src={appView.produto.imagem} className=" rounded float-left" width={200} alt="" />
                 </div>
                 <div className="col-6 ">
-                <h1 className="card-title">{appView.produto.nome}</h1> 
+                <h1 className="card-title mb-5">{appView.produto.nome}</h1> 
                 <h1 className="card-title">{appView.produto.price}</h1>
                 </div>
             </div>  
-            <div class="form-check offset-6">
+            <div className="form-check offset-6 ">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
                 <label class="form-check-label" for="flexCheckDefault">
                     Correios
@@ -29,9 +29,31 @@ const Vendas = (props) => {
                     Azul Cargo
                 </label>
                 <div class="form-check offset-6">
-                        <button type="button" class="btn btn-secondary">Secondary</button>
+                        <button type="button" className="btn btn-secondary mt-5 mb-5">Calcular Frete</button>
                      </div>
-                 </div>
+            </div>
+            <div className="offset-6 col-6 justify-content-center ">
+                <h1 className="card-title mb-3">Valor do Frete: 10,00 R$</h1> 
+                <h1 className="card-title mb-5">Valor Total: 14,90 R$</h1> 
+            </div>
+            <div className="offset-6 col-6 d-flex justify-content-center ">
+                <h1 className="card-title mb-5">Quantidade: </h1> 
+                <div className="col-4">
+            <div className="d-flex justify-content-between align-items-center border border-1 rounded-pill">
+                <i className="bi bi-dash-lg fs-5 btn"></i>
+                <span>0</span>
+                <i className="bi bi-plus-lg fs-5 btn"></i>
+                    </div>
+                
+                </div>
+                
+            </div>
+            <div>
+                <h1 className="card-title">Descrição: O produto mede 4cm quadrado, é feito de acrilico e sera embalado para não haver danos.</h1>
+            </div>
+            <div>
+                <button type="button" class="btn btn-secondary btn-lg offset-5 mb-5 mt-5">Adicionar ao Carrinho</button>
+            </div>
             </div>
         
     );
