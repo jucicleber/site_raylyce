3549e77b388f0f5578f0d4561c1275cc7d1eb5f9
jucicleber <71895344+jucicleber@users.noreply.github.com>
2022-08-09 19:09:59 -0400

    Update PosVendas.js

--- src/componente/Produtos/PosVendas.js
+++ src/componente/Produtos/PosVendas.js
@@ -48,7 +48,24 @@ const [numero, setNumero] = useState(0)
         </div>
                
     <hr />         
-           
+          <div className="d-flex justify-content-center">
+                <div className="col-6">   
+                     <input className="form-check-input ml-5" type="checkbox" value="" />         
+                    <img src={appView.produto.imagem} className=" rounded float-left" width={100} alt="" />  
+                 <div className="col-4 offset-md-5 d-flex align-items-center justify-content-center border border-1 rounded-pill">
+                    <i className="bi bi-dash-lg fs-5 btn" onClick={diminuir}></i>
+                    <span id="numero">{numero}</span>
+                    <i className="bi bi-plus-lg fs-5 btn" onClick={aumentar}></i>
+                    </div>
+                    
+            </div> 
+        <div className="col-6">
+            <h1 className="card-title mb-5">{appView.produto.nome}</h1> 
+            <h2 className="card-title mb-3">Valor item: {appView.produto.price}</h2>
+                    <h2 className="card-title">Valor do frete: 10.90 R$</h2>
+                    <button type="button" className="btn btn-secondary btn-lg offset-5 mb-5 mt-5 mr-2">Apagar item</button>
+        </div> 
+        </div> 
             
     
 </div>
