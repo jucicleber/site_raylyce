cc370654f098937b17c2d130c05f681476ff9b38
jucicleber <71895344+jucicleber@users.noreply.github.com>
2022-08-12 15:43:25 -0400

    att

--- src/componente/Administrativo/adicionar_produto.js
+++ src/componente/Administrativo/adicionar_produto.js
@@ -6,19 +6,24 @@ const AdicionarProduto = () => {
       <h1 className="mb-5">Inserir Produto!</h1>
       <h3 className="fs-1">Digite o nome do Produto.</h3>
       <input
-        className="input-group-text"
+        className="input-group-text mb-5"
         placeholder="Digite o nome do produto."
       ></input>
       <h3 className="fs-1">Digite o valor do Produto.</h3>
       <input
-        className="input-group-text"
+        className="input-group-text mb-5"
         placeholder="Digite o valor do produto."
       ></input>
       <h3 className="fs-1">Digite o quantidade do produto.</h3>
       <input
         type="tel"
-        className="input-group-text"
+        className="input-group-text mb-5"
         placeholder="Digite a quantidade em estoque hoje."
+      ></input>
+      <h3 className="fs-1 mb-5">No botão "Escolher arquivo carregue a imagem do produto."</h3>
+      <input
+        type="file"
+        className="myFile mb-5" 
       ></input>
       <h3 className="fs-1">Selecione o tipo do produto.</h3>
       <div class="form-check">
@@ -53,9 +58,10 @@ const AdicionarProduto = () => {
         <label class="fs-4 form-check-label" for="flexRadioDefault1">
           Missanguinhas
         </label>
+      
       </div>
       <button
-        className="btn btn-outline-primary me-5 px-5 "
+        className="col-6 offset-3 btn btn-outline-primary me-5 px-5 mb-5 mt-5"
         align="center"
         type="button"
       >
