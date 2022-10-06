65796d719b955cb5b67b7739cf46875916197267
jucicleber <71895344+jucicleber@users.noreply.github.com>
2022-10-06 11:49:49 -0400

    Update rastreio.js

--- src/componente/rastreio/rastreio.js
+++ src/componente/rastreio/rastreio.js
@@ -6,10 +6,11 @@ const Rastreio = () => {
       <div className="card">
         <nav class="navbar navbar-light bg-light">
           <form class="form-inline">
-            
+            <h1 className="btn-outline-warning ">Rastreio:</h1>
+            <h1 className="btn-outline-warning">Voce deseja rastrear um Pedido ou Encomenda enviado pro qual meio de transporte:</h1>
             <a href="https://rastreamento.correios.com.br/app/index.php">
               <button
-                class="btn btn-outline-warning btn-lg fs-1 me-1 rounded-pill"
+                className="btn btn-outline-warning btn-lg fs-1 me-1 mt-5  rounded-pill"
                 type="button"
               >
                 Rastreio Correios
@@ -17,7 +18,7 @@ const Rastreio = () => {
             </a>
             <a href="https://www.azulcargoexpress.com.br/Rastreio/Rastreio">
               <button
-                class="btn btn-outline-warning btn-lg fs-1 me-1 rounded-pill"
+                class="btn btn-outline-warning btn-lg fs-1 me-1 mt-5  rounded-pill"
                 type="button"
               >
                 Rastreio Azul Cargo
@@ -27,25 +28,8 @@ const Rastreio = () => {
         </nav>
       </div>
       <div className="d-flex justify-content-center">
-        <h1 className="h1 text-warning">
-          Rastrear Encomendas, por favor digite o número do pedido:
-        </h1>
-        <div className="card">
-          <input
-            type="email"
-            className=" form-control "
-            id="floatingInput"
-            placeholder="Digite seu código de compra."
-          />
-          <button
-            class="btn btn-outline-warning btn-lg fs-1 me-1 rounded-pill"
-            type="button"
-          >
-            Rastrear encomenda/Pedido
-          </button>
         </div>
       </div>
-    </div>
   );
 };
 
