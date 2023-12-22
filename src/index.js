0c45823581f156422d518c3129e5237c9c73d21c
Juice <juciclebersantos@gmail.com>
2023-12-22 15:18:49 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+//teste para subir o código na versão 
+
+
+console.log('teste feito')
+console.log('teste feito2')
+console.log('teste feito3')
+console.log('teste feito4')
+console.log('teste feito5')
+console.log('teste feito6')
+console.log('teste feito7')
+varconsole.log('teste feito8')
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
